36b6fced7a1963411a2da0d12d80c2161f352db3
Natalie-Cardot <nataliecardot@gmail.com>
2021-03-20 11:09:27 -0700

    Show clock, enable toggle date

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # React Clock
 
-A React clock built using these packages: [moment](https://www.npmjs.com/package/moment) (a JavaScript date library for parsing, validating, manipulating, and formatting dates), [antd](https://www.npmjs.com/package/antd) (Ant Design, a React UI library), and [react-live-clock](https://www.npmjs.com/package/react-live-clock) (a React clock with time zones).
+A digital clock with a date that can be toggled on and off built with React class-based components.
+
+A React clock built using these packages: [moment](https://www.npmjs.com/package/moment) (a JavaScript date library for parsing, validating, manipulating, and formatting dates), [antd](https://www.npmjs.com/package/antd) (Ant Design, a React UI library), [react-live-clock](https://www.npmjs.com/package/react-live-clock) (a React clock with time zones), react-moment, and moment-timezone, needed for react-live-clock to work.
---
+++ src/components/Clock.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import Clock from 'react-live-clock';
+import { Switch } from 'antd';
+import moment from 'moment';
+import './Clock.css';
+
+class Time extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     todaysDate: false,
+  //   };
+  // }
+  // Using class field declaration https://www.robinwieruch.de/react-state-without-constructor
+  state = { todaysDate: false };
+
+  // Also public class fields syntax. Eliminates the need to manually bind this in the contructor See https://www.peterbe.com/plog/public-class-fields and https://egghead.io/lessons/javascript-public-class-fields-with-react-components - public class fields: name of property + equals operator + expression we want to have it initialized to when each instance is being initialized
+  toggleSwitch = () => {
+    this.setState({
+      todaysDate: !this.state.todaysDate,
+    });
+  };
+
+  render() {
+    return (
+      <div className="Time">
+        <div className="toggleButton">
+          <Switch onClick={this.toggleSwitch} />
+        </div>
+        <Clock format={'h:mm:ssa'} ticking={true} timezone={'US/Pacific'} />
+        <div className="toggleDate">
+          {this.state.todaysDate && moment().format('MMMM Do YYYY')}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Time;
