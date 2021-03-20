3d8e7b77cae395613d51b425d07ac7e018170724
Natalie-Cardot <nataliecardot@gmail.com>
2021-03-20 11:46:32 -0700

    Add CSS

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -18,10 +18,13 @@ class Time extends Component {
   render() {
     return (
       <div className="Time">
-        <div className="toggleButton">
-          <Switch onClick={this.toggleSwitch} />
+        <div className="navbar">
+          React Clock
+          <Switch onClick={this.toggleSwitch} className="toggle" />
         </div>
-        <Clock format={'h:mm:ssa'} ticking={true} timezone={'US/Pacific'} />
+        <div className="time">
+          <Clock format={'h:mm:ssa'} ticking={true} timezone={'US/Pacific'} />
+        </div>
         <div className="toggleDate">
           {this.state.todaysDate && moment().format('MMMM Do YYYY')}
         </div>
